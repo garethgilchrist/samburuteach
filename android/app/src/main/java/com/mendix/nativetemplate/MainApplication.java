48f8187834e97233f32c9214df8a35e97d2b8d14
garethgilchrist <74386683+garethgilchrist@users.noreply.github.com>
2020-12-29 14:26:40 +0000

    trying again

--- android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
+++ android/app/src/main/java/com/mendix/nativetemplate/MainApplication.java
@@ -14,6 +14,7 @@ import org.devio.rn.splashscreen.SplashScreen;
 
 import java.util.List;
 import community.revteltech.nfc.NfcManagerPackage;
+import com.dooboolab.RNAudioRecorderPlayerPackage;
 
 public class MainApplication extends MendixReactApplication {
     @Override
